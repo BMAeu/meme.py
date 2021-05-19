6c7743ed5df6f44555f037d4372a3c5f5c7d85d0
BMAeu <50963113+BMAeu@users.noreply.github.com>
2021-05-19 20:33:11 -0300

    Add files via upload

---
+++ meme.py
@@ -0,0 +1,19 @@
+#!/usr/env/python
+
+import sys
+import os
+import time
+import random
+
+path = sys.argv[1]
+
+fileList = os.listdir(path)
+
+for f in fileList:  
+    name, extension = os.path.splitext(path + f)
+    file_creation = time.gmtime(os.path.getctime(path + f))
+
+    try:
+        os.rename(path + f, path + "Mem_" + time.strftime('%Y-%m-%d', file_creation) + "{}".format(extension) )
+    except:
+        os.rename(path + f, path + "Mem_" + time.strftime('%Y-%m-%d', file_creation)+ "({})".format(round(random.random(), 5)) + "{}".format(extension) )
